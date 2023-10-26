4349bdf0d3ddc9071ca72168b560b1e7185d1643
stanislavkravciv <kravcivs717@gmail.com>
2023-10-26 18:28:08 +0300

    make task597

--- 597/task.py
+++ 597/task.py
@@ -1,1 +1,21 @@
-print("hello")
+s = input()
+is_everything_Ok = True
+x, y = 0, 0
+lst = {(x, y)}
+for item in s:
+    if item == "N":
+        x, y = x, y + 1
+    elif item == "S":
+        x, y = x, y - 1
+    elif item == "W":
+        x, y = x + 1, y
+    elif item == "E":
+        x, y = x - 1, y
+    t = (x, y)
+    if t in lst:
+        print("Something goes wrong...")
+        is_everything_Ok = False
+        break
+    lst.add((x, y))
+if is_everything_Ok:
+    print("Looks OK.")
